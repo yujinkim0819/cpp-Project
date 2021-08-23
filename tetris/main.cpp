a033823eb88b32b9955cc4615d78a472c873d9be
yujinkim0819 <a76363282@gmail.com>
2021-08-23 23:51:38 +0900

    테트리스 메인 화면

--- tetris/main.cpp
+++
@@ -1,6 +0,0 @@
-#include <iostream>
-int main() {
-	int a = 10;
-	return 0;
-	// 테트리스 프로젝트
-}
---
+++ tetris/tetris.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	system("mode con cols=100 lines=50 | title tetris");
+	//getchar();
+	return 0;
+}
